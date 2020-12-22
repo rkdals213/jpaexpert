1a6994f408c7c4b5d638507b8aa8e73b1532fc43
rkdals213 <rkdals213@naver.com>
2020-12-22 19:09:38 +0900

    de

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -16,15 +16,17 @@ public class Category {
     @GeneratedValue
     @Column(name = "category_id")
     private Long id;
+
     private String name;
+
     @ManyToMany
-    @JoinTable(name = "category_item",
-            joinColumns = @JoinColumn(name = "category_id"),
-            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    @JoinTable(name = "category_item", joinColumns = @JoinColumn(name = "category_id"), inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
+
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
